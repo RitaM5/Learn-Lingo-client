142944f9138788ac9f3a5d09a064f798e444c261
RitaM5 <sporshia108514orva@gmail.com>
2023-06-09 02:17:53 +0600

    popular instructor section attached in home

---
+++ src/pages/Home/Home/PopularInstructor.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+
+const PopularInstructor = () => {
+    const [instructors, setInstructors] = useState([]);
+    const [axiosSecure] = useAxiosSecure()
+    useEffect(() => {
+        axiosSecure.get('/instructors')
+            .then(res => {
+                setInstructors(res.data)
+            })
+    }, []);
+    return (
+        <div className='my-16'>
+            <h1 className='text-center text-3xl font-poppins font-semibold text-pink-500 my-12'>Popular Instructors</h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-5'>
+                {instructors.map((instructor) => (
+                    <div className="card w-full bg-base-100 shadow-xl font-poppins">
+                        <figure><img src={instructor?.instructorImage} alt="Shoes" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">
+                                {instructor?.instructorName}
+                                <div className="badge badge-secondary">popular</div>
+                            </h2>
+                            <p><span className='text-lg font-semibold'>Instructor Email:</span> {instructor?.instructorEmail}</p>
+                            <p><span className='text-lg font-semibold'>Class Name:</span> {instructor?.className}</p>
+                            <p className=""><span className='text-lg font-semibold'>Students:</span> {instructor?.enrolled}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default PopularInstructor;
