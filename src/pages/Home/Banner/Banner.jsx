2e84dd80f7bc4f37cbcc274d14445c2f4d398320
RitaM5 <sporshia108514orva@gmail.com>
2023-06-11 12:12:20 +0600

    carousel added in instruct page

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -3,36 +3,22 @@ import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 import img1 from '../../../assets/english.jpg';
-import img2 from '../../../assets/chinese.jpg';
-import img3 from '../../../assets/japanese.jpg';
-import img4 from '../../../assets/korean.jpg';
+import img2 from '../../../assets/constructor/instruc-banner-1.jpg';
+import img3 from '../../../assets/constructor/instruc-banner-2.jpg';
+import img4 from '../../../assets/constructor/instruc-banner-3.jpg';
 
 const Banner = () => {
     return (
         <Carousel>
-            <div>
-                <img src={img1} />
-                <div className="flex text-left w-full items-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] absolute left-0 top-0 h-full">
-                    <div className=''>
-                        <div className='text-white ml-2 space-y-5 w-full p-4 '>
-                            <h2 className='text-5xl space-x-3 font-poppins'>
-                                <span className='text-pink-500'>#</span>4 Language Learning Website<br />and App for Students
-                            </h2>
-                            <p className=' font-poppins text-lg'>Online language learning games, videos, songs, books, worksheets and flashcards for students.!</p>
-                            <button className="bg-pink-600 px-3 py-2 rounded-md">Know More</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <div>
                 <img src={img2} />
                 <div className="flex text-left w-full items-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] absolute left-0 top-0 h-full">
                     <div className=''>
                         <div className='text-white ml-2 space-y-5 w-full p-4 '>
                             <h2 className='text-5xl space-x-3 font-poppins'>
-                                <span className='text-pink-500'>#</span>4 Language Learning Website<br />and App for Kids
+                                <span className='text-pink-500'>#</span>4 Language Learning Website<br />and App for Students
                             </h2>
-                            <p className=' font-poppins text-lg'>Online language learning games, videos, songs, books, worksheets and flashcards for children.!</p>
+                            <p className='w-1/2 font-poppins text-lg'>Online language learning games, videos, songs, books, worksheets and flashcards for students.!</p>
                             <button className="bg-pink-600 px-3 py-2 rounded-md">Know More</button>
                         </div>
                     </div>
@@ -44,9 +30,9 @@ const Banner = () => {
                     <div className=''>
                         <div className='text-white ml-2 space-y-5 w-full p-4 '>
                             <h2 className='text-5xl space-x-3 font-poppins'>
-                                <span className='text-pink-500'>#</span>4 Language Learning Website<br />and App for Students
+                                <span className='text-pink-500'>#</span>4 Language Learning Website<br />and App for Kids
                             </h2>
-                            <p className=' font-poppins text-lg'>Teachers love Dinolingo reporting tools !!</p>
+                            <p className='w-1/2  font-poppins text-lg'>Online language learning games, videos, songs, books, worksheets and flashcards for children.!</p>
                             <button className="bg-pink-600 px-3 py-2 rounded-md">Know More</button>
                         </div>
                     </div>
@@ -58,9 +44,23 @@ const Banner = () => {
                     <div className=''>
                         <div className='text-white ml-2 space-y-5 w-full p-4 '>
                             <h2 className='text-5xl space-x-3 font-poppins'>
+                                <span className='text-pink-500'>#</span>4 Language Learning Website<br />and App for Students
+                            </h2>
+                            <p className='w-1/2 font-poppins text-lg'>Teachers love Dinolingo reporting tools !!</p>
+                            <button className="bg-pink-600 px-3 py-2 rounded-md">Know More</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <img src={img1} />
+                <div className="flex text-left w-full items-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] absolute left-0 top-0 h-full">
+                    <div className=''>
+                        <div className='text-white ml-2 space-y-5 w-full p-4 '>
+                            <h2 className='text-5xl space-x-3 font-poppins'>
                                 <span className='text-pink-500'>#</span>4 Language Learning Website<br />and App for Kids
                             </h2>
-                            <p className=' font-poppins text-lg'>Online language learning games, videos, songs, books, worksheets and flashcards for children.!</p>
+                            <p className='w-1/2 font-poppins text-lg'>Online language learning games, videos, songs, books, worksheets and flashcards for children.!</p>
                             <button className="bg-pink-600 px-3 py-2 rounded-md">Know More</button>
                         </div>
                     </div>
--- src/pages/Home/Instructor/Instructor.jsx
+++ src/pages/Home/Instructor/Instructor.jsx
@@ -1,9 +1,62 @@
 import React from 'react';
-
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import banner1 from "../../../assets/constructor/instruc-banner-1.jpg"
+import banner2 from "../../../assets/constructor/instruc-banner-2.jpg"
+import banner3 from "../../../assets/constructor/instruc-banner-3.jpg"
 const Instructor = () => {
     return (
-        <div>
-            
+        <div className='my-container'>
+            <Carousel>
+                <div>
+                    <img src={banner1} />
+                    <div className="flex text-center w-full items-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] absolute left-0 top-0 h-full">
+                        <div className='text-center mx-auto'>
+                            <div className='text-white ml-2  space-y-5 w-full p-4 '>
+                                <h2 className='text-5xl font-semibold space-x-3 font-poppins'>
+                                    Our Real Heroes
+                                </h2>
+                                <p className='w-1/2 font-poppins text-lg mx-auto'>
+                                    All credit belongs to them, because they are very hardworking and educated. They constantly take care os the students and teach them different languages to take their future one step further towards succes.
+                                </p>
+                                <button className="bg-pink-600 px-3 py-2 rounded-md">Know More</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <img src={banner2} />
+                    <div className="flex text-center w-full items-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] absolute left-0 top-0 h-full">
+                        <div className='text-center mx-auto'>
+                            <div className='text-white ml-2  space-y-5 w-full p-4 '>
+                                <h2 className='text-5xl font-semibold space-x-3 font-poppins'>
+                                    Our Real Heroes
+                                </h2>
+                                <p className='w-1/2 font-poppins text-lg mx-auto'>
+                                    All credit belongs to them, because they are very hardworking and educated. They constantly take care os the students and teach them different languages to take their future one step further towards succes.
+                                </p>
+                                <button className="bg-pink-600 px-3 py-2 rounded-md">Know More</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <img src={banner3} />
+                    <div className="flex text-center w-full items-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] absolute left-0 top-0 h-full">
+                        <div className='text-center mx-auto'>
+                            <div className='text-white ml-2  space-y-5 w-full p-4 '>
+                                <h2 className='text-5xl font-semibold space-x-3 font-poppins'>
+                                    Our Real Heroes
+                                </h2>
+                                <p className='w-1/2 font-poppins text-lg mx-auto'>
+                                    All credit belongs to them, because they are very hardworking and educated. They constantly take care os the students and teach them different languages to take their future one step further towards succes.
+                                </p>
+                                <button className="bg-pink-600 px-3 py-2 rounded-md">Know More</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Carousel>
         </div>
     );
 };
